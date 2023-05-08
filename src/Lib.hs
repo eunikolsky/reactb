5913ede72827bba9374ad6464df071d5518f0e31
Eugene <e@egeek.me>
2023-05-08 16:10:22 +0300

    Allow to post and get orders

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,7 +6,10 @@ module Lib
     , app
     ) where
 
+import Control.Concurrent.MVar
+import Control.Monad.IO.Class
 import Data.Aeson
+import Data.Aeson qualified as A
 import Data.Aeson.TH
 import Data.Map.Strict (Map)
 import Data.Map.Strict qualified as M
@@ -14,7 +17,9 @@ import Network.Wai
 import Network.Wai.Handler.Warp
 import Network.Wai.Middleware.RequestLogger
 import Network.Wai.Middleware.Cors
+import Network.Wai.Middleware.Servant.Options
 import Servant
+import Test.RandomStrings
 
 data Meal = Meal
   { name :: String
@@ -31,19 +36,54 @@ newtype Meals = Meals (Map MealKey Meal)
 
 $(deriveJSON defaultOptions ''Meals)
 
+type OrderKey = String
+type Order = A.Value
+
+newtype Orders = Orders (Map OrderKey Order)
+  deriving stock (Eq, Show)
+
+$(deriveJSON defaultOptions ''Orders)
+
 type API = "meals.json" :> Get '[JSON] Meals
+  -- TODO in fact, firebase returns the key of the added object
+  :<|> "orders.json" :> ReqBody '[JSON] A.Value :> Post '[JSON] NoContent
+  :<|> "orders.json" :> Get '[JSON] Orders
+
+type RuntimeState = MVar Orders
 
 startApp :: IO ()
-startApp = run 8080 app
+startApp = do
+  state <- newMVar $ Orders M.empty
+  run 8080 $ app state
 
-app :: Application
-app = logStdoutDev $ simpleCors $ serve api server
+app :: RuntimeState -> Application
+app = logStdoutDev
+  . cors (const $ Just policy)
+  -- this is needed to support CORS preflight requests
+  . provideOptions api
+  . serve api
+  . server
+  where
+    policy = simpleCorsResourcePolicy { corsRequestHeaders = ["content-type"] }
 
 api :: Proxy API
 api = Proxy
 
-server :: Server API
-server = return defaultMeals
+server :: RuntimeState -> Server API
+server state = getMeals :<|> postOrder state :<|> getOrders state
+
+getMeals :: Handler Meals
+getMeals = return defaultMeals
+
+postOrder :: RuntimeState -> A.Value -> Handler NoContent
+postOrder state order = liftIO $ do
+  key <- randomWord (onlyAlphaNum randomASCII) 20
+  (Orders orders) <- takeMVar state
+  putMVar state . Orders $ M.insert key order orders
+  return NoContent
+
+getOrders :: RuntimeState -> Handler Orders
+getOrders state = liftIO $ readMVar state
 
 defaultMeals :: Meals
 defaultMeals = Meals $ M.fromList
